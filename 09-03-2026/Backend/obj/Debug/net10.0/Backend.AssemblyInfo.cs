b93f85bbbb5712e05c707da21e37529404b5ad79
abc <abc@gmil.com>
2026-03-11 10:36:09 +0530

    Employee Leave MS

--- 09-03-2026/Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ 09-03-2026/Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d40f14d0a217fd0fdf67a9bdaea8955ffa656268")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb7c827a0372ec51479707cc7a530402e3829dd4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
